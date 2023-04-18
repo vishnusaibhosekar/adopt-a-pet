017a123b09e70db5e438af8c57a9758b858989a5
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2023-04-18 03:03:15 -0400

    adds search functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ const appRouter = createBrowserRouter(createRoutesFromElements(
     <Route index element={ <HomePage/> } />
     <Route path=':type' element={ <HomePage/> } />
     <Route path=':type/:id' element={ <PetDetailsPage/> } />
+    <Route path='search' element={ <SearchPage/> } />
+    <Route path='pet-details-not-found' element={ <PetDetailsNotFound/> } />
   </Route>
 ));
 
